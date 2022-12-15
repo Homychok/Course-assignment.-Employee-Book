16b8c60fb8d15618006ff9d0cf9bff9a38aaed17
Alexandra <alexandravolchihina@yandex.ru>
2022-12-15 15:29:40 +0300

    Курсовой проект сделан

--- src/Employee.java
+++ src/Employee.java
@@ -1,2 +1,47 @@
 public class Employee {
+    private int department;
+    private String fullName;
+    private int id;
+   private double salary;
+    private static int count = 0;
+
+    private int totalSalary;
+
+    public Employee(String fullName, int department, double salary) {
+        this.fullName = fullName;
+        this.department = department;
+        this.salary = salary;
+        id = ++count;
+    }
+
+    public int getId() {
+        return id;
+    }
+        public String getFullName() {
+        return this.fullName;
+    }
+    public int getDepartment() {
+        return this.department;
+    }
+    public double getSalary() {
+        return this.salary;
+    }
+    public void setSalary(double salary) {
+        this.salary = salary;
+    }
+    public void setDepartment(int department) {
+        this.department = department;
+    }
+
+    @Override
+    public String toString() {
+        return "Сотрудник id " + id + ". " + fullName + ". " + department + ". " + salary;
+    }
+
+    public void setTotalSalary(int totalSalary) {
+        this.totalSalary = totalSalary;
+    }
 }
+
+
+
--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,30 @@
+import java.util.Arrays;
+
 public class Main {
-    public static void main(String[] args) {
-        System.out.println("Hello world!");
+    public static void printemployees(Employee[] employees) {
+        for (Employee employee : employees) {
+            System.out.println(employee);
+        }
     }
+    public static void main(String[] args) {
+        Employee[] employees = new Employee[10];
+        employees[0] = new Employee("Ivanov Ivan Ivanovich", 1, 10_000);
+        employees[1] = new Employee("Petrov Peter Petrovich", 2, 15_000);
+        employees[2] = new Employee("Frolov Ivan Nikolaevich", 3, 20_000);
+        employees[3] = new Employee("Kuznetsov Ivan Petrovich", 1, 40_000);
+        employees[4] = new Employee("Sokolov Artem Vasilevich", 2, 60_000);
+        employees[5] = new Employee("Kuznetsov Ivan Ivanovich", 3, 10_000);
+        employees[6] = new Employee("Kuznetsov Peter Petrovich", 1, 70_000);
+        employees[7] = new Employee("Frolov Peter Nikolaevich", 2, 50_000);
+        employees[8] = new Employee("Frolov Ivan Petrovich", 3, 40_000);
+        employees[9] = new Employee("Frolov Peter Ivanovich", 2, 30_000);
+        printemployees(employees);
+        System.out.println(employees.length + " человек всего в компании");
+        int[] department = {1, 2, 3};
+
+    }
+
+
+
 }
+
